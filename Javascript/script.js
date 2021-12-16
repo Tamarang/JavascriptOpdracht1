b998ca2bb51572104b856ab8167a166ab6edb52f
Jan Siepel <91777292+Tamarang@users.noreply.github.com>
2021-12-16 18:53:38 +0100

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+
+    let name = "Jan";
+    let number1 = 5;
+    let number2 = 2;
+    let age = "51";
+
+    console.log(name);
+    console.log(5 + 2);
+    console.log(5 - 2);
+    console.log(5 * 2);
+    console.log(5 / 2);
+    console.log(5 % 2);
+    console.log(typeof(age));
